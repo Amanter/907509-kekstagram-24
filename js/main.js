f540630d9f3df767c02d69cfc4dcbe7878dff545
Amanter <mister_x82@mail.ru>
2021-10-12 13:40:52 +0300

    generate Array

--- js/main.js
+++ js/main.js
@@ -9,9 +9,82 @@ const getRandomInt = function (min, max) {
 
 getRandomInt(0, 100);
 
-const getStringLenth = function (string, maxLength) {
-  const stringLenth = string.split('');
-  return stringLenth.length > maxLength;
+const getStringLenth = (string, maxLength) => string.length <= maxLength;
+getStringLenth('find string length', 5);
+
+const getArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
+
+let unitNumber = 0;
+
+const getNumber = (quantity) => {
+  for (;;) {
+    if (unitNumber < quantity) {
+      return unitNumber++;
+    }
+  }
 };
 
-getStringLenth('find string length', 5);
+const getComment = (array) => {
+  if (getRandomInt(1,2)===2) {
+    return `${getArrayElement(array)} ${getArrayElement(array)}`;
+  }
+  return getArrayElement(array);
+};
+
+const ARRAY_NUMBER = 25;
+const ID_NUMBER = 150;
+
+const DESCRIPTIONS = [
+  'Фотография природы',
+  'красиво озеро',
+  'Универмаг',
+  'Детская площадка',
+  'Лодочная станция',
+  'ресторан',
+  'Торговый зал',
+];
+
+const NAMES = [
+  'Андрей',
+  'Алексей',
+  'Ирина',
+  'Максим',
+  'Евгений',
+  'Дарья',
+  'Мария',
+  'Иосиф',
+  'Сергей',
+  'Александр',
+];
+
+const COMMENT = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+
+const getPhotoDescription = () => {
+  const description = {
+    id: getNumber(ARRAY_NUMBER),
+    url: `photos/${getNumber(ARRAY_NUMBER)}.jpg`,
+    description: getArrayElement(DESCRIPTIONS),
+    likes: getRandomInt(15, 200),
+    comment: [
+      {
+        id: getNumber(ID_NUMBER),
+        avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+        message: getComment(COMMENT),
+        name: getArrayElement(NAMES),
+      },
+    ],
+  };
+  return description;
+};
+
+const photoDescription = Array.from({length: ARRAY_NUMBER}, getPhotoDescription());
+photoDescription;
