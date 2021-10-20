b41f87c4fc30d15ef6d4d3518092a4b9994eb4f7
Amanter <mister_x82@mail.ru>
2021-10-20 14:46:28 +0300

    rendering Photo element

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ import {ARRAY_NUMBER, getPhotoDescription} from './data.js';
 import {createPhotoDescription} from './render-pic.js';
 
 const photoDescription = Array.from({length: ARRAY_NUMBER}, (value, num) => getPhotoDescription(num++));
-photoDescription;
-createPhotoDescription();
 
-export {photoDescription};
+// eslint-disable-next-line no-console
+console.log(photoDescription);
+
+photoDescription.forEach((arr) => createPhotoDescription(arr));
+
--- js/render-pic.js
+++ js/render-pic.js
@@ -1,17 +1,14 @@
-import {photoDescription} from './main.js';
-
 const pictureContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const fragment = document.createDocumentFragment();
 
-const createPhotoDescription = photoDescription;
-createPhotoDescription.forEach((photoArr) => {
+const createPhotoDescription = ({url, likes, comments}) => {
   const photoElement = pictureTemplate.cloneNode(true);
-  photoElement.querySelector('.picture__img').src = photoArr.url;
-  photoElement.querySelector('.picture__likes').textContent = photoArr.likes;
-  photoElement.querySelector('.picture__comments').textContent = photoArr.comments;
+  photoElement.querySelector('.picture__img').src = url;
+  photoElement.querySelector('.picture__likes').textContent = likes;
+  photoElement.querySelector('.picture__comments').textContent = comments.length;
   fragment.appendChild(photoElement);
-});
-pictureContainer.appendChild(fragment);
+  pictureContainer.appendChild(fragment);
+};
 
 export {createPhotoDescription};
