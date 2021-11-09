9086963ef1670b511e1259db68cad21805f427cc
Amanter <mister_x82@mail.ru>
2021-11-09 08:17:31 +0300

    create effects

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 import {photoDescription} from './data.js';
 import {createBigPicture} from './render-big-pic.js';
+import {setPictureScale} from './scale-picture.js';
 import './render-pic.js';
 import './form-validation.js';
 import './render-big-pic.js';
+import './effect-filter.js';
+
+setPictureScale(100);
 
 createBigPicture(photoDescription[1]);
